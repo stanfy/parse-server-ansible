e39caf8487ed92809c8adff26d824b9349ad4803
alen15 <evdorado@gmail.com>
2017-03-01 12:06:47 +0200

    Update newrelic.js

--- templates/default-project/newrelic.js
+++ templates/default-project/newrelic.js
@@ -1,7 +1,7 @@
 'use strict'
 exports.config = {
     app_name: ['APPNAME'],
-    license_key: 'ffc817673729c5f48da308a6641d388a31b18783',
+    license_key: 'your_key',
     logging: {
         level: 'info'
     }
